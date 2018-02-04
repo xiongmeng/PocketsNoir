83711de40cfcff8a655cde38802edf505a34b2f9
xiongmeng <277309623@qq.com>
2018-02-04 16:17:49 +0800

    调整会员等级计算金额底线

--- app/Jobs/RecalculateVip.php
+++ app/Jobs/RecalculateVip.php
@@ -30,8 +30,7 @@ class RecalculateVip extends Job
             $vip = new Vip();
             $vip->mobile = $mobile;
             $vip->card = Vip::CARD_1;
-            $res = $vip->save();
-            \Log::info("DEBUG_RECALCULATE_" . 'Save1', ['res' => $res, 'vip' => $vip->toArray()]);
+            $vip->save();
 
             $vip = Vip::find($this->mobile);
         }
@@ -46,14 +45,8 @@ class RecalculateVip extends Job
         if(!empty($map)){
             $youZanTrades = YouZanService::getTradeListByYouZanAccountId($map->yz_uid);
             foreach ($youZanTrades as $youZanTrade){
-                \Log::info("DEBUG_RECALCULATE_" . "GotTrade", $youZanTrade);
-                \Log::info("DEBUG_RECALCULATE_" . "GotTradeStatus:" . $youZanTrade['status']);
-                \Log::info("DEBUG_RECALCULATE_" . "GotTradePayment:" . $youZanTrade['payment']);
-
                 if($youZanTrade['status'] == 'TRADE_BUYER_SIGNED'){
-                    \Log::info("DEBUG_RECALCULATE_" . "GotTradeAddMoneyBefore:" . $consumeYouZan);
                     $consumeYouZan += $youZanTrade['payment'];
-                    \Log::info("DEBUG_RECALCULATE_" . "GotTradeAddMoneyAfter:" . $consumeYouZan);
                 }
             }
         }
@@ -70,27 +63,21 @@ class RecalculateVip extends Job
         \Log::info("DEBUG_RECALCULATE_" . "SumMoneyBefore:" ,['consume' => $consume, 'consumeYouZan' => $consumeYouZan, 'consumeGuanJiaPo' => $consumeGuanJiaPo]);
 
         $targetVip = Vip::CARD_1;
-        if($consume >= 1500){
+        if($consume >= 3000){
 //        if($consume > 0){
             $targetVip = Vip::CARD_2;
-        }elseif ($consume >= 5000){
+        }elseif ($consume >= 10000){
             $targetVip = Vip::CARD_3;
         }elseif ($consume >= 50000){
             $targetVip = Vip::CARD_4;
         }elseif ($consume >= 100000){
             $targetVip = Vip::CARD_5;
         }
-        \Log::info("DEBUG_RECALCULATE_" . "Calc1:" ,['targetVip' => $targetVip, 'consume' => $consume,
-            'consumeYouZan' => $consumeYouZan, 'consumeGuanJiaPo' => $consumeGuanJiaPo,
-            'vip_card' => $vip->card, 'vip_manual_marked' => $vip->manual_marked
-        ]);
 
 //        如果目标会员卡低于当前卡级别，而且此卡是人工设定（众筹员工奖励），则不降级
         if($targetVip < $vip->card && $vip->manual_marked){
             $targetVip = $vip->card;
         }
-
-        \Log::info("DEBUG_RECALCULATE_" . "Calc2:" ,['targetVip' => $targetVip, 'consume' => $consume, 'consumeYouZan' => $consumeYouZan, 'consumeGuanJiaPo' => $consumeGuanJiaPo, 'vip_card' => $vip->card, 'vip_manual_marked' => $vip->manual_marked]);
 
         $vip->consumes = $consume;
         $vip->consumes_youzan = $consumeYouZan;
@@ -98,11 +85,8 @@ class RecalculateVip extends Job
         if($targetVip <> $vip->card){
             $vip->card = $targetVip;
         }
-        \Log::info("DEBUG_RECALCULATE_" . "Calc3:" ,['targetVip' => $targetVip, 'consume' => $consume, 'consumeYouZan' => $consumeYouZan, 'consumeGuanJiaPo' => $consumeGuanJiaPo, 'vip_card' => $vip->card, 'vip_manual_marked' => $vip->manual_marked]);
 
-        $res = $vip->save();
-
-        \Log::info("DEBUG_RECALCULATE_" . 'Save2', ['res' => $res, 'vip' => $vip->toArray()]);
+        $vip->save();
 
         /**
          * 同步卡
