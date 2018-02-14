7719d8b65c947374f806b4c0b14ce13a187a7586
xiongmeng <277309623@qq.com>
2018-02-14 11:28:45 +0800

    打包后添加版本号

--- resources/views/2018chunjie/entry.blade.php
+++ resources/views/2018chunjie/entry.blade.php
@@ -45,5 +45,6 @@
         }
         var num = new Date().getTime();
     </script>
-    <script src="/js/app.js?v=201802141020" type="text/javascript"></script>
+<!--    <script src="/js/app.js?v=201802141020" type="text/javascript"></script>-->
+    <script src="{{mix('js/app.js')}}" type="text/javascript"></script>
 </html>
